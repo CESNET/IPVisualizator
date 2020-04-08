11ca4f6c321eb4c5a597a25cfc0d5f7747256f1e
Jakub Jancicka <jancijak@fit.cvut.cz>
2020-04-08 11:33:23 +0200

    change version of python package

--- IPVizualizator-python/setup.py
+++ IPVizualizator-python/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='IPVizualizator',
-    version='0.1',
+    version='0.11',
     description='IPVizualizator is an interactive heatmap which represents values assigned to IP addresses using Hilbert curve.',
     author='Jakub Jancicka',
     author_email='jancijak@fit.cvut.cz',
